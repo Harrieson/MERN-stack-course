2f3c0133816f729e22f875d9eda588fe00a750e3
Harrieson <harriemburu@gmail.com>
2023-01-22 23:49:30 +0300

    Setting up store

--- front-end/src/features/users/usersApiSlices.js
+++
@@ -1,6 +0,0 @@
-import { createSelector, createEntityAdapter } from "@reduxjs/toolkit";
-import { apiSlice } from "../../app/api/apiSlice";
-
-
-const usersAdapter = createEntityAdapter({})
-const initialState = usersAdapter.getInitialState()
