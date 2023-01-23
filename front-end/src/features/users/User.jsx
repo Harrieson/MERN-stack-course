9a524d7ab938098ff717a7ca8c3761c5593a13de
Harrieson <harriemburu@gmail.com>
2023-01-23 17:59:55 +0300

    Connect back end to front end

---
+++ front-end/src/features/users/User.jsx
@@ -0,0 +1,31 @@
+import { fontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { selectUserById } from './usersApiSlices'
+
+import React from 'react'
+
+const User = ({ userId }) => {
+    const user = useSelector(state => selectUserById(state, userId))
+    const navigate = useNavigate()
+    if (user) {
+        const handleEdit = () => navigate(`/dashboard/users/${userId}`)
+        const userRolesString = user.roles.toString().replaceAll(',', ',')
+        const cellStatus = user.active ? '' : 'table__cell--inactive'
+
+        return (
+            <tr className="table__row user">
+                <td className={`table__cell ${cellStatus}`}>{user.username}</td>
+                <td className={`table__cell ${cellStatus}`}>{userRolesString}</td>
+                <td className={`table__cell ${cellStatus}`}>
+                    <button className="icon-button table__button" onClick={handleEdit}>
+                        <fontAwesomeIcon icon={faPenToSquare} />
+                    </button>
+                </td>
+            </tr>
+        )
+    } else return null
+}
+
+export default User
--- front-end/src/features/users/UserList.jsx
+++ front-end/src/features/users/UserList.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { useGetUsersQuery } from './usersApiSlices'
+import User from './User'
 
 const UserList = () => {
-    return (
-        <div>UserList</div>
-    )
+    const { data: users,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetUsersQuery()
+    let content
+
+    if (isLoading) content = <p>Loading..</p>
+    if (isError) {
+        content = <p className={isError ? 'errmsg' : 'offscreen'}>{error?.data.message}</p>
+    }
+    if (isSuccess) {
+        const { ids } = users
+        const tableContent = ids?.length
+            ? ids.map(userId => <User key={userId} userId={userId} />)
+            : null
+        content = (
+            <table className='table, table--users'>
+                <thead className='table__thead'>
+                    <tr>
+                        <th scope='col' className='table__th user__username'>Username</th>
+                        <th scope='col' className='table__th user__roles'>Roles</th>
+                        <th scope='col' className='table__th user__edit'>Edit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableContent}
+                </tbody>
+            </table>
+        )
+    }
+    return content
 }
 
 export default UserList
